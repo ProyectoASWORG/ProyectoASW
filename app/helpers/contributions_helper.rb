5dc12716b4aa1045ff452fa4dde47c4e835e8360
dani <dannirodriguez99@gmail.com>
2021-10-28 01:00:07 +0200

    #21 Mejoras logica votacion

--- app/helpers/contributions_helper.rb
+++ app/helpers/contributions_helper.rb
@@ -1,7 +1,10 @@
 module ContributionsHelper
     def liked_contribution(contribution_id)
+        if !user_signed_in?
+            return false
+        end
         if current_user.voted_contribution_ids.include?(contribution_id)
-            "visibility:hidden;"
+            true
         end
     end
 end
