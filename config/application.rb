e02d1e36bf95df493738552630b96a8a01abb2c9
dani <dannirodriguez99@gmail.com>
2021-11-06 15:17:04 +0100

    cors.rb

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative "boot"
 
 require "rails/all"
-require "rack/cors"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,12 +10,6 @@ module Waslab05
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.middleware.insert_before 0, "Rack::Cors" do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
