17370fb3b5505210c7d2ed32697c7b8769308220
Pau Sàbat Martí <pau.sabat.marti@estudiantat.upc.edu>
2021-11-08 12:33:30 +0100

    eliminado el mensaje al updatear valores del usr

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   def update 
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to users_edit_url(@user), notice: 'User was successfully updated.' }
+        format.html { redirect_to users_edit_url(@user)}
         format.json { render :edit, status: :ok, location: @user }
       else
         format.html { render :edit }
