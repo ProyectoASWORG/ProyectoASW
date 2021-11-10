43755a347ba93a1129e6621d51d242eb4753478d
dani <dannirodriguez99@gmail.com>
2021-11-10 20:41:10 +0100

    changes gitignore

---
+++ app/javascript/packs/manage-votations.js
@@ -0,0 +1,44 @@
+like_contribution = (id) =>{
+        var origin = window.location.origin;
+        var arrow = document.getElementById(id);
+        arrow.style.visibility = "hidden";
+        fetch(`${origin}/contributions/${id}/like`,{
+        method: 'PUT',
+        headers:{
+            'Content-Type':'application/json'
+        }
+        }).then(response =>{
+        if(response.statusText === "Unprocessable Entity"){
+            var url = `${origin}/users/sign_in`
+            location.replace(url);
+        }else{
+            var unvote = document.getElementById(`${id}_unvote`);
+            unvote.style.display = "block";
+            location.reload();
+        }
+        
+        })
+
+    }
+
+    dislike_contribution = (id) =>{
+        var origin = window.location.origin;
+        var arrow = document.getElementById(id);
+        arrow.style.visibility = "visible";
+        fetch(`${origin}/contributions/${id}/dislike`,{
+        method: 'PUT',
+        headers:{
+            'Content-Type':'application/json'
+        }
+        }).then(response =>{
+
+        if(response.statusText === "Unprocessable Entity"){
+            var url = `${origin}/users/sign_in`;
+            location.replace(url);
+        }else{
+            var unvote = document.getElementById(`${id}_unvote`);
+            unvote.style.display = "none";
+            location.reload();
+        }
+        })
+    }
