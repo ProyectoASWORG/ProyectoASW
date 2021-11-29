cc61e45b54915f5fb9418db2b133e35d23981980
Pau Sàbat Martí <pau.sabat.marti@estudiantat.upc.edu>
2021-11-29 17:34:14 +0100

    cambiando controlador de usuarios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,72 +3,92 @@ class UsersController < ApplicationController
 
   before_action :get_user, only: [:edit, :update]
 
+  #GET /users/1
+  #GET /users/1.json
+  def show
+  end
+
   def edit
     respond_to do |format|
+      #comporbar usuario autorizado
       if @user.nil?
-        respond_to do |format|
-          format.html { redirect_to :users, notice: 'You need to be logged in to edit your profile', status: :unauthorized }
-          format.json {
-            render json: {
+          format.html { redirect_to :contributions, notice: 'You need to be logged in to edit your profile', status: :unauthorized }
+          format.json { render json: {
               error: "user not found",
               status: :unauthorized
             }, status: :unauthorized
           }
-        end
       else
-        if @usuario.id == @user.id
-          format.html { render :edit }
-          format.json { render :edit, status: :ok, location: @user }
+        if @usuario.nil?
+        #comprobar que el usuario a modificar existe
+        format.html { redirect_to :contributions, notice: "The user that you're trying to edit does not exist", status: :not_found }
+        format.json { render json: {
+            error: "user not found",
+            status: :not_found
+          }, status: :not_found
+        }
         else
-          format.html { redirect_to users_show_url(@user) }
-          format.json { render json: @user.errors, status: :unprocessable_entity }
+          #comprobar que el usuario autorizado y el que se quiere actualizar son el mismo
+          if @usuario.id == @user.id
+            format.html { render :edit }
+            format.json { render json: {
+                msg: "user edited",
+                status: :ok,
+                user: @usuario 
+              }, status: :success
+            }
+          #el usuario autorizado y el que se quiere actualizar no son el mismo
+          else
+            format.html { redirect_to users_show_url(@usuario) }
+            format.json { render json: {
+                error: "Authorized user does not match with updatable user",
+                status: unprocessable_entity
+              }, status: unprocessable_entity
+            }
+          end
         end
       end
     end
   end
 
-  #GET /users/1
-  #GET /users/1.json
-  def show
-  end
 
   def update 
     respond_to do |format|
       #comprobar usuario autorizado
       if @user.nil?
           format.html { redirect_to :contributions, notice: 'You need to be logged in to update your profile', status: :unauthorized }
-          format.json {
-            render json: {
+          format.json { render json: {
               error: "user not authenticated",
               status: :unauthorized
             }, status: :unauthorized
           }
-        end
-      elsif @usuario.nil?
+      else
+        if @usuario.nil?
         #comprobar que el usuario a modificar existe
         format.html { redirect_to :contributions, notice: "The user that you're trying to upadte does not exist", status: :not_found }
-          format.json {
-            render json: {
-              error: "user not found",
-              status: :not_found
-            }, status: :not_found
-          }
-      else
-        if @usuario.id == @user.id and @usuario.update(user_params)
-          format.html { redirect_to users_edit_url(@usuario)}
-          format.json {
-            render json:{
-              msg: "User updated", 
-              user: @usuario
-            }, status: :success
-          }
-        else
-          format.html { render :edit }
-          format.json { render json: {
-            error: "Authorized user does not match with updatable user",
-            status: unprocessable_entity
-          }, status: unprocessable_entity
+        format.json { render json: {
+            error: "user not found",
+            status: :not_found
+          }, status: :not_found
         }
+        else
+          #comprobar que el usuario autorizado y el que se quiere actualizar son el mismo
+          if @usuario.id == @user.id and @usuario.update(user_params)
+            format.html { redirect_to users_edit_url(@usuario)}
+            format.json { render json:{
+                msg: "user updated", 
+                user: @usuario
+              }, status: :success
+            }
+          #el usuario autorizado y el que se quiere actualizar no son el mismo
+          else
+            format.html { render :edit }
+            format.json { render json: {
+                error: "Authorized user does not match with updatable user",
+                status: unprocessable_entity
+              }, status: unprocessable_entity
+            }
+          end
         end
       end
     end
