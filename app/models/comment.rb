780338ce8bff4c3431bec90f5f3a5a905d2a1cdc
mariianaSh <mariana.shabanova@gmail.com>
2021-11-30 20:31:20 +0100

    Cambiar triangulos por estrellas en los comments y replys propios y restringuir comentarios vacios

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :contribution
+  validates :text, presence: true
 
-  
+
 
   has_many :replays, class_name: "Comment", foreign_key: "replayedComment_id"
   belongs_to :replayedComment, class_name: "Comment", optional: true
